6f00f6c778016bb00de624390cb7f19958ef2736
Sharkyus <sharkyus379@gmail.com>
2018-07-06 01:20:46 +0300

    FIX: add access public dev server

--- front/config/webpack.dev.js
+++ front/config/webpack.dev.js
@@ -1,13 +1,14 @@
 let Merge = require('webpack-merge');
 let baseConfig = require('./webpack.base.js');
+const path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = Merge(baseConfig, {
     mode: 'development',
     devtool: 'source-map',
     devServer: {
-        // contentBase: path.join(__dirname, 'dist'),
-        port: 8080
+        port: 8080,
+        host: '0.0.0.0'
     },
 
     plugins: [new HtmlWebpackPlugin({template: 'index.html'})]
