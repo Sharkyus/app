66dee1bf79fa6b2246dd9311c9e4177b1261c1fc
Sharkyus <sharkyus379@gmail.com>
2018-07-10 01:45:47 +0300

    FIX: remove logs

--- front/config/webpack.dev.js
+++ front/config/webpack.dev.js
@@ -4,8 +4,6 @@ let config = require('./dev.json');
 const path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
-console.log(1111111111, config);
-
 module.exports = Merge(baseConfig, {
     mode: 'development',
     devtool: 'source-map',
