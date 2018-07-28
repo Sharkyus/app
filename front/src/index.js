f142bfb1f48fb6589e4579cfad2877a5c70cf897
Sharkyus <sharkyus379@gmail.com>
2018-07-28 20:20:06 +0300

    FEATURE: prepare redux

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,32 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const history = createBrowserHistory();
+
+const store = createStore(
+    connectRouter(history)(rootReducer),
+    compose(
+        applyMiddleware(
+            routerMiddleware(history)
+        ),
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route exact path="/" render={() => (<App/>)} />
+                <Route render={() => (<div>404</div>)} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('app')
+);
